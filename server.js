e86aec7cfc2c460f708152395e498a4ffeaf47a4
AnmolPatel22 <anmol.patel@smartsensesolutions.com>
2022-01-12 13:39:47 +0530

    Refactor api with _

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -11,19 +12,16 @@ app.get('/',function (req, res){
     res.send('TOdo API Root');
 });
 
+//GET / todos
 app.get('/todos',function(req, res){
     res.json(todos);
 });
 
+//GET /todos/:id
 app.get('/todos/:id',function(req, res){
     var todoId = parseInt(req.params.id,10);
-    var matchedTodo;
+    var matchedTodo  = _.findWhere(todos, {id: todoId});
 
-    todos.forEach(function(todo){
-        if(todoId === todo.id){
-            matchedTodo = todo;
-        }
-     });
      if(matchedTodo){
         res.json(matchedTodo);
      }else{
@@ -33,11 +31,16 @@ app.get('/todos/:id',function(req, res){
 
 //POST
 app.post('/todos',function(req,res){ 
-    var body = req.body;
-    console.log('Description : ' + body.description);
+    var body = _.pick(req.body,'description','complated');
+
+    if(!_.isBoolean(body.complated) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
     body.id = todoNextId++;
     todos.push(body);
-    console.log(todos);
+ 
     res.json(body);
 });
 
